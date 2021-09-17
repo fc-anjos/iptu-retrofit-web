cb334315f996b98e94ebdcd270c267a11850def5
fc-anjos <felipe.anjos@usp.br>
2021-09-17 18:19:10 +0000

    WIP: Read SHP

--- retrofit-web/src/App.js
+++ retrofit-web/src/App.js
@@ -3,31 +3,40 @@ import React from 'react';
 import DeckGL from '@deck.gl/react';
 import {LineLayer} from '@deck.gl/layers';
 import {StaticMap} from 'react-map-gl';
+import {ShapefileLoader} from '@loaders.gl/shapefile';
+import {load} from '@loaders.gl/core';
 
 // Set your mapbox access token here
-const MAPBOX_ACCESS_TOKEN = process.env.MAPBOX_ACCESS_TOKEN;
+const MAPBOX_ACCESS_TOKEN = process.env.REACT_APP_MAPBOX_ACCESS_TOKEN;
 console.log(MAPBOX_ACCESS_TOKEN);
 console.log(process.env);
 
+
+
 // Viewport settings
 const INITIAL_VIEW_STATE = {
-  longitude: -122.41669,
-  latitude: 37.7853,
-  zoom: 13,
+  longitude: -46.63,
+  latitude: -23.55,
+  zoom: 12,
   pitch: 0,
   bearing: 0
 };
 
-// Data to be used by the LineLayer
-const data = [
-  {sourcePosition: [-122.41669, 37.7853], targetPosition: [-122.41669, 37.781]}
-];
 
 function App({data}) {
   const layers = [
     new LineLayer({id: 'line-layer', data})
   ];
 
+  const layer = new Tile3DLayer({
+    id: 'tile-3d-layer',
+    // Tileset entry point: Indexed 3D layer file url
+    data: './data/perimetro.shp',
+    loader: ShapefileLoader,
+    onTilesetLoad: this._onTilesetLoad.bind(this)
+  });
+
+  
   return (
     <DeckGL
       initialViewState={INITIAL_VIEW_STATE}
