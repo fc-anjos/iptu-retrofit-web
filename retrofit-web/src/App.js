e559339bb3345823f53b2d6359085e0f4deec084
fc-anjos <felipe.anjos@usp.br>
2021-09-20 15:23:16 +0000

    Wrap App in a bootstrap container

--- retrofit-web/src/App.js
+++ retrofit-web/src/App.js
@@ -1,12 +1,14 @@
+import Container from 'react-bootstrap/Container';
+
 import MainMap from './components/MainMap';
 import Header from './components/Header';
 
 function App() {
   return (
-    <>
+    <Container>
       <Header></Header>
       <MainMap></MainMap>
-    </>
+    </Container>
   );
 }
 
